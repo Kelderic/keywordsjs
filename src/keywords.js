c06cabc10713a399b1f120b955159142173ea764
Andy Mercer <andymercer@outlook.com>
2017-12-05 15:22:48 -0500

    CSS Bugfix: Choices List Should be Above Keywords

--- src/keywords.js
+++ src/keywords.js
@@ -66,7 +66,7 @@
 			self.css.innerHTML += '.kwjs-outerwrap { position: relative; } ';
 			self.css.innerHTML += '.kwjs-keywordwrap { pointer-events: none; position: absolute; z-index:1; padding: 4px 0 4px 4px; } ';
 			self.css.innerHTML += '.kwjs-keyword { display: inline-block; font-size: inherit; font-weight: inherit; font-style: inherit; font-family: inherit; vertical-align:bottom; pointer-events: auto; position: relative; border-radius: 2px; box-sizing: border-box; margin-right: 4px; cursor: move } ';
-			self.css.innerHTML += '.kwjs-choiceswrap { position: absolute; top: calc(100% + 2px); left: 0; right: 0; z-index:1; max-height:300px; overflow:auto; list-style: none; margin:0; padding:0; transform: scaleY(0); transform-origin:top; transition:transform 0.2s } ';
+			self.css.innerHTML += '.kwjs-choiceswrap { position: absolute; top: calc(100% + 2px); left: 0; right: 0; z-index:2; max-height:300px; overflow:auto; list-style: none; margin:0; padding:0; transform: scaleY(0); transform-origin:top; transition:transform 0.2s } ';
 			self.css.innerHTML += '.kwjs-choiceswrap li { cursor:pointer; padding:5px } ';
 			self.css.innerHTML += '.kwjs-choiceswrap li:hover, li.kwjs-highlighted { background:white; } ';
 			self.css.innerHTML += 'li.kwjs-alreadychosen, li.kwjs-filteredout { display:none; } ';
