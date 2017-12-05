638d0f42841d985f6b1cd070c2ba1863fc0bb1b7
Andy Mercer <andymercer@outlook.com>
2017-12-05 16:28:26 -0500

    Only Create Style Element Once

--- src/keywords.js
+++ src/keywords.js
@@ -61,18 +61,29 @@
 
 			// SET UP CSS ELEMENT
 
-			self.css = document.createElement('style');
-			self.css.type = 'text/css';
-			self.css.innerHTML += '.kwjs-outerwrap { position: relative; } ';
-			self.css.innerHTML += '.kwjs-keywordwrap { pointer-events: none; position: absolute; z-index:1; padding: 4px 0 4px 4px; } ';
-			self.css.innerHTML += '.kwjs-keyword { display: inline-block; font-size: inherit; font-weight: inherit; font-style: inherit; font-family: inherit; vertical-align:bottom; pointer-events: auto; position: relative; border-radius: 2px; box-sizing: border-box; margin-right: 4px; cursor: move } ';
-			self.css.innerHTML += '.kwjs-choiceswrap { position: absolute; top: calc(100% + 2px); left: 0; right: 0; z-index:2; max-height:300px; overflow:auto; list-style: none; margin:0; padding:0; transform: scaleY(0); transform-origin:top; transition:transform 0.2s } ';
-			self.css.innerHTML += '.kwjs-choiceswrap li { cursor:pointer; padding:5px } ';
-			self.css.innerHTML += '.kwjs-choiceswrap li:hover, li.kwjs-highlighted { background:white; } ';
-			self.css.innerHTML += 'li.kwjs-alreadychosen, li.kwjs-filteredout { display:none; } ';
-			self.css.innerHTML += '.kwjs-focused .kwjs-choiceswrap { transform:none; } ';
-			
-			document.body.appendChild(self.css);
+			var existingCSS = document.querySelector('style#kwjs-css');
+
+			if ( existingCSS ) {
+
+				self.css = existingCSS;
+
+			} else {
+
+				self.css = document.createElement('style');
+				self.css.type = 'text/css';
+				self.css.id = 'kwjs-css';
+				self.css.innerHTML += '.kwjs-outerwrap { position: relative; } ';
+				self.css.innerHTML += '.kwjs-keywordwrap { pointer-events: none; position: absolute; z-index:1; padding: 4px 0 4px 4px; } ';
+				self.css.innerHTML += '.kwjs-keyword { display: inline-block; font-size: inherit; font-weight: inherit; font-style: inherit; font-family: inherit; vertical-align:bottom; pointer-events: auto; position: relative; border-radius: 2px; box-sizing: border-box; margin-right: 4px; cursor: move } ';
+				self.css.innerHTML += '.kwjs-choiceswrap { position: absolute; top: calc(100% + 2px); left: 0; right: 0; z-index:2; max-height:300px; overflow:auto; list-style: none; margin:0; padding:0; transform: scaleY(0); transform-origin:top; transition:transform 0.2s } ';
+				self.css.innerHTML += '.kwjs-choiceswrap li { cursor:pointer; padding:5px } ';
+				self.css.innerHTML += '.kwjs-choiceswrap li:hover, li.kwjs-highlighted { background:white; } ';
+				self.css.innerHTML += 'li.kwjs-alreadychosen, li.kwjs-filteredout { display:none; } ';
+				self.css.innerHTML += '.kwjs-focused .kwjs-choiceswrap { transform:none; } ';
+
+				document.body.appendChild(self.css);
+
+			}
 
 			// SET UP ELEMENT REFERENCES CONTAINER
 
